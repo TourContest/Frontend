846eadb9ec9cea09092df14972f72deb34c17d86
eunseopi <asxzqw12@naver.com>
2025-08-27 23:02:05 +0900

    카카오 맵 연동 및 캐릭터 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useCheckIn } from "src/features/main/useCheckIn";
 import CheckInModal from "src/components/modal/CheckInModal";
+import KaKaoMap from "src/components/map/KaKaoMap";
 
 export default function Main() {
   const { state, claim } = useCheckIn();
@@ -14,9 +15,7 @@ export default function Main() {
 
   return (
     <div>
-      {/* 여기는 일단 나중에.. */}
-      <h1>메인</h1>
-
+      <KaKaoMap appKey={import.meta.env.VITE_KAKAO_JS_KEY} />
       <CheckInModal
         open={open}
         day={state.day}
